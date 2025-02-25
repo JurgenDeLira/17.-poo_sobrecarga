ee536ab4dbda828924f4b6d62d4d954d46d6bdd5
JurgenDeLira <arq.jorgedelira@gmail.com>
2025-02-25 15:30:29 -0700

    Tercer commit

--- src/org/jdelira/poosobrecarga/Calculadora.java
+++ src/org/jdelira/poosobrecarga/Calculadora.java
@@ -1,12 +1,16 @@
 package org.jdelira.poosobrecarga;
 
+
 public class Calculadora {
 
-    public int sumar(int a, int b){
+    private Calculadora() {
+    }
+
+    public static int sumar(int a, int b){
         return a + b;
     }
 
-    public int sumar(int... argumentos){
+    public static int sumar(int... argumentos){
         int total = 0;
         for (int i: argumentos){
             total +=i;
@@ -14,7 +18,7 @@ public class Calculadora {
         return total;
     }
 
-    public float sumar(float a, int... argumentos){
+    public static float sumar(float a, int... argumentos){
         float total = a;
         for (int i: argumentos){
             total +=i;
@@ -22,7 +26,7 @@ public class Calculadora {
         return total;
     }
 
-    public  double sumar(double... varargs){
+    public static double sumar(double... varargs){
         double total = 0.0;
         for (double d: varargs){
             total += d;
@@ -30,23 +34,23 @@ public class Calculadora {
         return total;
     }
 
-    public float sumar(float x, float y){
+    public static float sumar(float x, float y){
         return x + y;
     }
 
-    public float sumar(int i, float j){
+    public static float sumar(int i, float j){
         return  i + j;
     }
 
-    public float sumar (float i, int j){
+    public static float sumar (float i, int j){
         return i + j;
     }
 
-    public double sumar(double a , double b){
+    public static double sumar(double a , double b){
         return a + b;
     }
 
-    public int sumar(String a, String b){
+    public static int sumar(String a, String b){
         int resultado;
         try {
             return Integer.parseInt(a) + Integer.parseInt(b);
@@ -56,7 +60,7 @@ public class Calculadora {
         return resultado;
     }
 
-    public int sumar(int a, int b, int c){
+    public static int sumar(int a, int b, int c){
         return a + b + c;
     }
 
